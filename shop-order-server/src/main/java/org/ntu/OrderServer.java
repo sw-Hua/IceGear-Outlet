ef26df276155f7b48edb8249e935c40bcac671bc
Songwen Hua <a13419460@163.com>
2023-12-15 00:08:25 +0800

    Initialized Building of Nacos Environment
    Initialized building of nacos environment

--- shop-order-server/src/main/java/org/ntu/OrderServer.java
+++ shop-order-server/src/main/java/org/ntu/OrderServer.java
@@ -1,7 +1,9 @@
 package org.ntu;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class OrderServer {
     public static void main(String[] args) {
         SpringApplication.run(OrderServer.class, args);
