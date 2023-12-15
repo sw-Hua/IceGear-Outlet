cc29312cf6c1d87703617799644b361849181bf2
Songwen Hua <a13419460@163.com>
2023-12-15 17:48:10 +0800

    First Try Balance Loader
    First Try Balance Loader

--- shop-order-server/src/main/java/org/ntu/service/imp/OrderServiceImpl.java
+++ shop-order-server/src/main/java/org/ntu/service/imp/OrderServiceImpl.java
@@ -7,7 +7,12 @@ import org.ntu.domain.Order;
 import org.ntu.domain.Product;
 import org.ntu.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -15,17 +20,35 @@ public class OrderServiceImpl implements IOrderService{
 
     @Autowired
     private OrderDao orderDao;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient; // 从nacos中获取服务列表（地址）
+    int i = 0;
     @Override
     public Order createOrder(Long productId, Long userId) {
         log.info("接收到{}号商品的下单请求,接下来调用商品微服务查询此商品信息", productId);
         //远程调用商品微服务,查询商品信息
-        Product product = null;
+        List<ServiceInstance> instances = discoveryClient.getInstances("product-service");
+        i++;
+
+        int index = i % instances.size();
+        ServiceInstance serviceInstance = instances.get(index);
+        String host = serviceInstance.getHost();
+        int port = serviceInstance.getPort();
+
+        // String url = String.format("http://%s:%s/product/%s", host, port, productId);
+        String url = "http://" + host + ":" + port + "/product/" + productId;
+        log.info("服务的地址是:{}", url);
+        Product product = restTemplate.getForObject(url, Product.class);
+
         log.info("查询到{}号商品的信息,内容是:{}", productId,
                 JSON.toJSONString(product));
         //创建订单并保存
         Order order = new Order();
         order.setUid(userId);
-        order.setUsername("叩丁狼教育");
+        order.setUsername("Songwen Hua");
         order.setPid(productId);
         order.setPname(product.getPname()); order.setPprice(product.getPprice());
         order.setNumber(1);
--- shop-product-server/src/main/java/org/ntu/controller/ProductController.java
+++ shop-product-server/src/main/java/org/ntu/controller/ProductController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.ntu.domain.Product;
 import org.ntu.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,15 @@ public class ProductController {
     @Autowired
     private IProductService productService;
 
+    @Value("${server.port}")
+    private String port;
+
     //商品信息查询
     @RequestMapping("/product/{pid}")
     public Product findByPid(@PathVariable("pid") Long pid) {
         log.info("接下来要进行{}号商品信息的查询", pid);
         Product product = productService.findByPid(pid);
+        product.setPname(product.getPid() + " data from port=" + port);
         log.info("商品信息查询成功,内容为{}", JSON.toJSONString(product));
         return product;
     }
