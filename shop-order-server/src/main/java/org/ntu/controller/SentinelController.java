b5c5942e00afdefd4d2c324e9ff7b30db514dffc
Songwen Hua <a13419460@163.com>
2023-12-16 23:59:16 +0800

    Sentinel Function Update
    Sentinel Function Update

---
+++ shop-order-server/src/main/java/org/ntu/controller/SentinelController.java
@@ -0,0 +1,20 @@
+package org.ntu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class SentinelController {
+    @RequestMapping("/sentinel1")
+    public String sentinel1() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(1);
+        return "sentinel1";
+    }
+
+    @RequestMapping("/sentinel2")
+    public String sentinel2() throws InterruptedException{
+        return "sentinel2";
+    }
+}
